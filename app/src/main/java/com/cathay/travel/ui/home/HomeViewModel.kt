ca933becd7b4dff5a36ed04d24fb1c5e654f5468
Daniel <kuanshenwang@gmail.com>
2024-09-10 11:03:53 +0800

    Create ViewModel

---
+++ app/src/main/java/com/cathay/travel/ui/home/HomeViewModel.kt
@@ -0,0 +1,66 @@
+package com.cathay.travel.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cathay.travel.data.Resource
+import com.cathay.travel.data.news.News
+import com.cathay.travel.data.place.Place
+import com.cathay.travel.repository.ITravelRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val travelRepository: ITravelRepository
+): ViewModel() {
+    private val _newsListLiveData = MutableLiveData<List<News>>()
+    val newsListLiveData: LiveData<List<News>> get() = _newsListLiveData
+
+    private val _placeListLiveData = MutableLiveData<List<Place>>()
+    val placeListLiveData: LiveData<List<Place>> get() = _placeListLiveData
+
+    private val _loadingLiveData = MutableLiveData(false)
+    val loadingLiveData: LiveData<Boolean> get() = _loadingLiveData
+
+    fun getNews() {
+        viewModelScope.launch {
+            travelRepository.getNews(lang = "zh-tw").collect { response ->
+                when(response) {
+                    is Resource.Success -> {
+                        _newsListLiveData.value = response.data?.newsList ?: listOf()
+                        _loadingLiveData.value = false
+                    }
+                    is Resource.Loading -> {
+                        _loadingLiveData.value = true
+                    }
+                    else -> {
+                        _loadingLiveData.value = false
+                    }
+                }
+            }
+        }
+    }
+
+    fun getPlaceList() {
+        viewModelScope.launch {
+            travelRepository.getPlaceList(lang = "zh-tw").collect { response ->
+                when(response) {
+                    is Resource.Success -> {
+                        _placeListLiveData.value = response.data?.placeList ?: listOf()
+                        _loadingLiveData.value = false
+                    }
+                    is Resource.Loading -> {
+                        _loadingLiveData.value = true
+                    }
+                    else -> {
+                        _loadingLiveData.value = false
+                    }
+                }
+            }
+        }
+    }
+
+}
