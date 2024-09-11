a851f5c2c0d4a467562e9c0cd848f500f0393801
Daniel <kuanshenwang@gmail.com>
2024-09-11 21:56:12 +0800

    Set navigation from place to webView

--- app/src/main/java/com/cathay/travel/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/cathay/travel/ui/place/PlaceFragment.kt
@@ -16,17 +16,15 @@ import com.cathay.travel.ui.home.HomeViewModel
 import com.cathay.travel.ui.place.adapter.PhotoPagerAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * 景點頁面
+ */
 @AndroidEntryPoint
 class PlaceFragment : Fragment() {
     private var _binding: FragmentPlaceBinding? = null
     private val binding get() = _binding!!
-    val viewModel: HomeViewModel by activityViewModels()
-    val photoAdapter: PhotoPagerAdapter by lazy { PhotoPagerAdapter() }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private val viewModel: HomeViewModel by activityViewModels()
+    private val photoAdapter: PhotoPagerAdapter by lazy { PhotoPagerAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,48 +45,88 @@ class PlaceFragment : Fragment() {
         val place = viewModel.selectedPlaceLiveData.value
         place?.let {
             setActionBarTitle(it.name)
-            setContent(place)
+            setOpenTime(place)
+            setAddress(place)
+            setTelephone(place)
+            setOfficialSite(place)
+            setIntroduction(place)
+            setPhotoPager(place)
         }
     }
 
+    /**
+     * 設定標題
+     */
     private fun setActionBarTitle(title: String) {
         (activity as AppCompatActivity).supportActionBar?.title = title
     }
 
-    private fun setContent(place: Place) {
-        // Open time
+    /**
+     * 設定營業時間
+     */
+    private fun setOpenTime(place: Place) {
         binding.openTimeCard.visibility =
             if (place.openTime.isNullOrEmpty()) View.GONE else View.VISIBLE
         place.openTime?.let {
-            binding.openTimeTv.text =
-                String.format(getString(R.string.label_open_time), it)
+            binding.openTimeTv.text = String.format(getString(R.string.label_open_time), it)
         }
-        // Address
+    }
+
+    /**
+     * 設定地址
+     */
+    private fun setAddress(place: Place) {
         binding.addressCard.visibility =
             if (place.address.isNullOrEmpty()) View.GONE else View.VISIBLE
         place.address?.let {
-            binding.addressTv.text =
-                String.format(getString(R.string.label_address), it)
+            binding.addressTv.text = String.format(getString(R.string.label_address), it)
         }
-        // Telephone
+    }
+
+    /**
+     * 設定電話
+     */
+    private fun setTelephone(place: Place) {
         binding.telephoneCard.visibility =
             if (place.tel.isNullOrEmpty()) View.GONE else View.VISIBLE
         place.tel?.let {
-            binding.telephoneTv.text =
-                String.format(getString(R.string.label_telephone), it)
+            binding.telephoneTv.text = String.format(getString(R.string.label_telephone), it)
         }
-        // Official site
+    }
+
+    /**
+     * 設定網站
+     */
+    private fun setOfficialSite(place: Place) {
         binding.officialSiteCard.visibility =
             if (place.officialSite.isNullOrEmpty()) View.GONE else View.VISIBLE
-        place.officialSite?.let {
-            binding.officialSiteTv.text =
-                String.format(getString(R.string.label_official_site), it)
+        place.officialSite?.let { url ->
+            val formatUrl = String.format(getString(R.string.label_official_site), url)
+            binding.officialSiteTv.text = Html.fromHtml(formatUrl, Html.FROM_HTML_MODE_LEGACY)
+            binding.officialSiteTv.setOnClickListener {
+                findNavController().navigate(
+                    PlaceFragmentDirections.actionPlaceToWeb(
+                        url = url,
+                        title = place.name
+                    )
+                )
+            }
         }
-        // Introduction
+    }
+
+    /**
+     * 設定介紹內文
+     */
+    private fun setIntroduction(place: Place) {
         place.introduction?.let {
             binding.introTv.text = Html.fromHtml(it, Html.FROM_HTML_MODE_LEGACY)
         }
-        // Photo Pager
+    }
+
+    /**
+     * 設定相片
+     */
+    private fun setPhotoPager(place: Place) {
         val imageUrlList = place.images?.mapNotNull { it.src }
         binding.photoVp.visibility =
             if (imageUrlList.isNullOrEmpty()) View.GONE else View.VISIBLE
