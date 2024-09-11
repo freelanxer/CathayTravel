d109f573283acdab7fe2dda435520a15e4e3ad22
Daniel <kuanshenwang@gmail.com>
2024-09-11 20:33:07 +0800

    Add Event model to handle one time LiveData

---
+++ app/src/main/java/com/cathay/travel/model/EventObserver.kt
@@ -0,0 +1,13 @@
+package com.cathay.travel.model
+
+import androidx.lifecycle.Observer
+
+class EventObserver<T>(
+    private val onEventUnhandledContent: (T) -> Unit
+) : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>) {
+        event.getContentIfNotHandled()?.let { value ->
+            onEventUnhandledContent(value)
+        }
+    }
+}
