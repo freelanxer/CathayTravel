d3ef21c9eafa5ce9ce570716ad58861a7a12776e
Daniel <kuanshenwang@gmail.com>
2024-09-11 23:19:58 +0800

    Support back action in WebView

--- app/src/main/java/com/cathay/travel/extension/ComponentExt.kt
+++ app/src/main/java/com/cathay/travel/extension/ComponentExt.kt
@@ -1,9 +1,47 @@
 package com.cathay.travel.extension
 
+import android.app.Activity
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 
 fun <T> LifecycleOwner.observe(liveData: LiveData<T>, action: (t: T) -> Unit) {
     liveData.observe(this, Observer { it?.let { t -> action(t) } })
+}
+
+fun Activity.onBackButtonPressed(callback: (() -> Boolean)) {
+    (this as? FragmentActivity)?.onBackPressedDispatcher?.addCallback(
+        owner = this,
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (!callback()) {
+                    remove()
+                    performBackPress()
+                }
+            }
+        })
+}
+
+fun Activity.performBackPress() {
+    (this as? FragmentActivity)?.onBackPressedDispatcher?.onBackPressed()
+}
+
+fun Fragment.onBackButtonPressed(callback: () -> Boolean) {
+    requireActivity().onBackPressedDispatcher.addCallback(
+        viewLifecycleOwner,
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (!callback()) {
+                    remove()
+                    performBackPress()
+                }
+            }
+        })
+}
+
+fun Fragment.performBackPress() {
+    requireActivity().onBackPressedDispatcher.onBackPressed()
 }
--- app/src/main/java/com/cathay/travel/ui/web/WebViewFragment.kt
+++ app/src/main/java/com/cathay/travel/ui/web/WebViewFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
 import com.cathay.travel.databinding.FragmentWebViewBinding
+import com.cathay.travel.extension.onBackButtonPressed
 import com.cathay.travel.ui.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -34,6 +35,11 @@ class WebViewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        onBackButtonPressed {
+            val canGoBack = binding.webView.canGoBack()
+            binding.webView.goBack()
+            canGoBack
+        }
         setWebViewConfig()
     }
 
